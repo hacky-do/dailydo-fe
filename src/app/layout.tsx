e76c72ae5d595b1de17e9f36ebeb250c499b68d9
sohxxny <sohxxny@gmail.com>
2026-05-12 10:11:56 +0900

    chore: 폰트 및 파비콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: 'Daily:DO',
 };
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+});
 
 export default function RootLayout({
   children,
@@ -12,8 +18,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={`h-full antialiased`}>
-      <body className="flex min-h-full flex-col">{children}</body>
+    <html lang="ko" className={`${pretendard.variable} h-full antialiased`}>
+      <body className="h-full">
+        <div className="mx-auto flex h-full max-w-107.5 min-w-90 flex-col">
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
