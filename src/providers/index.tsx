293f9fb3e729efdf8dc0ff7f80f7a916f36019ac
bluemistmark <bluemistmark@gmail.com>
2026-05-21 19:08:51 +0900

    style: default export 대신 named export로 변경

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -3,10 +3,10 @@ import { ReactNode } from 'react';
 import { MSWProvider } from './msw-provider';
 import { ReactQueryProvider } from './react-query-provider';
 
-export default function Providers({ children }: { children: ReactNode }) {
+export const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <ReactQueryProvider>
       <MSWProvider>{children}</MSWProvider>
     </ReactQueryProvider>
   );
-}
+};
