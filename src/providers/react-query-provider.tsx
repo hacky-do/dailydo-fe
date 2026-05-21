1990c737f135ec6a253a0f40225efc21b7f6f899
bluemistmark <bluemistmark@gmail.com>
2026-05-21 19:08:51 +0900

    style: default export 대신 named export로 변경

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -3,8 +3,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode, useState } from 'react';
 
-export function ReactQueryProvider({ children }: { children: ReactNode }) {
+export const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-}
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
