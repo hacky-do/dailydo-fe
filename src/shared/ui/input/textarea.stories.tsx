0e8102b24b1ea35bfced8a77e3612a189f968f4d
sohxxny <sohxxny@gmail.com>
2026-05-22 17:09:09 +0900

    fix: 스토리에 label 추가 (#17)

--- src/shared/ui/input/textarea.stories.tsx
+++ src/shared/ui/input/textarea.stories.tsx
@@ -10,12 +10,24 @@ const meta: Meta<typeof Textarea> = {
     placeholder: 'Input text',
     description: 'Description',
   },
+  argTypes: {
+    id: { table: { disable: true } },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Textarea>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: (args) => (
+    <div className="w-full">
+      <label className="sr-only" htmlFor="textarea-default">
+        Label
+      </label>
+      <Textarea {...args} id="textarea-default" />
+    </div>
+  ),
+};
 
 export const Interactive: Story = {
   args: {
@@ -30,13 +42,19 @@ export const Interactive: Story = {
     const MAX = 100;
 
     return (
-      <Textarea
-        {...args}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        maxLength={MAX}
-        description={`${value.length}/${MAX}자`}
-      />
+      <div className="w-full">
+        <label className="sr-only" htmlFor="textarea-interactive">
+          Label
+        </label>
+        <Textarea
+          {...args}
+          id="textarea-interactive"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          maxLength={MAX}
+          description={`${value.length}/${MAX}자`}
+        />
+      </div>
     );
   },
 };
