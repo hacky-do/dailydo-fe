523df25f83016291fda0ccaeb4bc371a3d1fd005
bluemistmark <bluemistmark@gmail.com>
2026-05-24 13:12:52 +0900

    fix: 옵션 전달받도록 수정

--- src/shared/lib/use-auth-query.ts
+++ src/shared/lib/use-auth-query.ts
@@ -1,4 +1,7 @@
-import { useBaseQuery } from './use-base-query';
+import { BaseQueryOptions, useBaseQuery } from './use-base-query';
 
-export const useAuthQuery = <T>(queryKey: string[], endpoint: string) =>
-  useBaseQuery<T>(queryKey, endpoint, { retry: 2 });
+export const useAuthQuery = <T>(
+  queryKey: string[],
+  endpoint: string,
+  options?: BaseQueryOptions<T>,
+) => useBaseQuery<T>(queryKey, endpoint, { retry: 2, ...options });
