6ea67335a9b01276149e60c6bfdb07275e3d9851
rondido <rppr001@naver.com>
2026-05-27 11:34:50 +0900

    feat: page return 및 readme.md 파일 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { MissionList } from '@/widgets/missionList';
-
 export default function Home() {
-  return (
-    <div>
-      <h1>오늘의 미션</h1>
-      <MissionList />
-    </div>
-  );
+  return null;
 }
