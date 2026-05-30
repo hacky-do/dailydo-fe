531adcb9923225466b10a7deaade90193ec69b57
rondido <rppr001@naver.com>
2026-05-30 20:21:24 +0900

    feat: 리뷰 슈정(공통 fetch), deafult style

--- src/entities/missions/model/useMissions.ts
+++ src/entities/missions/model/useMissions.ts
@@ -1,18 +1,13 @@
 import { useEffect, useState } from 'react';
 
 import { Mission } from '@/entities/missions/model/mission.types';
-import { BASE_URL } from '@/shared/api/base-url.constant';
+import { clientApi } from '@/shared/api/fetch-client';
 
 export const useMissions = () => {
   const [missions, setMissions] = useState<Mission[]>([]);
 
   useEffect(() => {
-    const fetchMissions = async () => {
-      const res = await fetch(`${BASE_URL}/api/missions/new`);
-      const data = await res.json();
-      setMissions(data);
-    };
-    fetchMissions();
+    clientApi.get<Mission[]>('/api/missions/new').then(setMissions);
   }, []);
 
   return missions;
