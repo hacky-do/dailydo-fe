7e5c7d43410840cc71f2ebcccd0210c5e3c89a6f
sohxxny <sohxxny@gmail.com>
2026-06-05 11:03:48 +0900

    feat: 마이로그 페이지 calendar 구조 변경 및 목데이터 추가

--- src/views/mylogs/ui/mylogs-page.tsx
+++ src/views/mylogs/ui/mylogs-page.tsx
@@ -1,3 +1,44 @@
+'use client';
+
+import { useLayoutEffect, useRef } from 'react';
+
+import { Calendar } from '@/features/mylogs';
+import { MOCK_LOG_RECORDS } from '@/features/mylogs/model/mylogs.mock';
+import { Loader } from '@/shared/ui/loader';
+
 export const Mylogs = () => {
-  return <div className="h-full w-full bg-white p-5"></div>;
+  const containerRef = useRef<HTMLUListElement>(null);
+  const loaderRef = useRef<HTMLDivElement>(null);
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  // 스크롤이 아래부터 시작, flash 방지
+  useLayoutEffect(() => {
+    bottomRef.current?.scrollIntoView();
+    bottomRef.current?.focus({ preventScroll: true });
+    containerRef.current?.classList.remove('invisible');
+    loaderRef.current?.classList.add('hidden');
+  }, []);
+
+  return (
+    <div className="scrollbar-hide relative h-full w-full overflow-y-auto bg-white p-5">
+      <div
+        ref={loaderRef}
+        className="absolute inset-0 flex items-center justify-center bg-white"
+      >
+        <Loader />
+      </div>
+      <ul ref={containerRef} className="invisible divide-y divide-gray-100">
+        {MOCK_LOG_RECORDS.map((record) => (
+          <li className="py-8" key={`${record.year}-${record.month}`}>
+            <Calendar
+              year={record.year}
+              month={record.month}
+              logs={record.logs}
+            />
+          </li>
+        ))}
+      </ul>
+      <div ref={bottomRef} tabIndex={-1} />
+    </div>
+  );
 };
