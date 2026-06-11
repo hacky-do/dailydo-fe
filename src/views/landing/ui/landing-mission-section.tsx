17bcbf18ba772306df4331748754291a4fa746d1
bluemistmark <bluemistmark@gmail.com>
2026-06-11 09:38:12 +0900

    design: bg 색상 변경

--- src/views/landing/ui/landing-mission-section.tsx
+++ src/views/landing/ui/landing-mission-section.tsx
@@ -5,7 +5,7 @@ import { MissionCarousel } from './mission-carousel';
 
 export const LandingMissionSection = () => {
   return (
-    <section className="relative bg-white py-16">
+    <section className="relative bg-gray-50 pt-30 pb-40">
       <div className="px-5">
         <FadeIn>
           <SectionHeading
