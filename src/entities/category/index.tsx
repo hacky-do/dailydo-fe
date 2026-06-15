10f0d985efebf162c4405327a2ba140b02249dd7
sohxxny <sohxxny@gmail.com>
2026-06-15 17:07:55 +0900

    feat: 카테고리 카드 선택 API 추가
    # Conflicts:
    #	src/entities/category/api/category.api.ts
    #	src/entities/category/api/category.queries.ts
    #	src/entities/category/index.tsx
    #	src/entities/category/model/category.constants.ts
    #	src/entities/category/model/types.ts

--- src/entities/category/index.tsx
+++ src/entities/category/index.tsx
@@ -1,4 +1,3 @@
-export { getMissionCategories } from './api/category.api';
 export { useGetMissionCategories } from './api/category.queries';
 export { categoryQueryKeys } from './model/category.constants';
-export type { Category, MissionCategoriesResponse } from './model/types';
+export type { Category, CategoryListResponse } from './model/category.types';
